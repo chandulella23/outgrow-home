eceeb5e731d8aeffd97eb4b289651945d399e965
Sahil Verma <sahil.verma@venturepact.com>
2016-09-29 13:19:45 +0530

    [Text change ]

--- js/plans.js
+++ js/plans.js
@@ -48,7 +48,7 @@ jQuery(document).ready(function() {
 							<button onclick="callGA('CANNOT WAIT CTA')" class="btn-buildcal mk-animate-element fade-in"><i class="material-icons">view_compact</i> I Cannot Wait to Build One!</button>
 						</a>
 						<div class="btn-bottominfo mk-animate-element fade-in">
-							<span>Free Plan Available</span>
+							<span>Free Trial Available</span>
 							<label>No Credit Card Required</label>
 						</div>`;
 	jQuery('#btnBuildCalc').html(btn);
