3665ec23b3f8455cfba43cf26f0e291c3220cc14
Naman Kareer <naman.kareer@venturepact.com>
2016-11-03 19:43:39 +0530

    check onresize

--- js/iloader1.js
+++ js/iloader1.js
@@ -14,6 +14,6 @@ e.appendChild(iFrame);
 //ogOnLoadHandler
  var ogOnLoadHandler = document.createElement("script");
  ogOnLoadHandler.setAttribute("type", "text/javascript");
- var node = document.createTextNode("function ogOnLoadHandler(){var screenWidth = screen.width; var screenHeight = screen.height; var aspectRatio = screenWidth/screenHeight; var width = document.getElementById(\"og-iframe\").clientWidth; var height = width/aspectRatio; document.getElementById(\"og-iframe\").style.height = height+'px';}");
+ var node = document.createTextNode("function ogOnLoadHandler(){console.log('In Func'); var screenWidth = screen.width; var screenHeight = screen.height; var aspectRatio = screenWidth/screenHeight; var width = document.getElementById(\"og-iframe\").clientWidth; var height = width/aspectRatio; document.getElementById(\"og-iframe\").style.height = height+'px';}");
  ogOnLoadHandler.appendChild(node);
  e.appendChild(ogOnLoadHandler);
