faf78affcb79087621462bff645a8fe32ba1d0ca
Sahil Verma <sahil.verma@venturepact.com>
2016-11-08 22:10:34 +0530

    [Homepage] : add 2 buttons

--- js/plans.js
+++ js/plans.js
@@ -52,6 +52,18 @@ jQuery(document).ready(function() {
 						</div>`;
 	jQuery('#btnBuildCalc').html(btn);
 
+	var btn1 = `<a href="http://app.outgrow.`+ext+`" class="params">
+
+							<button onclick="callGA('CANNOT WAIT CTA')" class="btn-buildcal mk-animate-element fade-in"><i class="material-icons">view_compact</i>Let's Build One!</button>
+						</a>
+						<a href="javascript:0(void);" class="params">
+
+							<button onclick="togglePopup()" class="btn-buildcal red mk-animate-element fade-in"><i class="material-icons">view_compact</i>Calculate ROI</button>
+						</a>
+						<div class="btn-bottominfo mk-animate-element fade-in">
+						</div>`;
+	jQuery('#btnBuildCalc1').html(btn1);
+
 });
 
 var getAllPlans = function () {
