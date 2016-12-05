f64d8a5b61c31ffdc7eced943824c4d6f35494b3
Naman Kareer <naman.kareer@venturepact.com>
2016-12-05 12:31:59 +0530

    check

--- js/fullscreen/iloader.js
+++ js/fullscreen/iloader.js
@@ -14,14 +14,14 @@ og_iFrame.setAttribute("id", "og-iframe");
 og_iFrame.setAttribute("src", og_u);
 og_iFrame.setAttribute("width", og_w);
 og_iFrame.setAttribute("scrolling", "auto");
-og_iFrame.setAttribute("class", "height-auto");
+//og_iFrame.setAttribute("class", "height-auto");
 
 //Resize button for mobile
 var iWidth = window.innerWidth;
 if(iWidth < 786) {
 	//CSS
 	var og_style = document.createElement("style");
-	var style_node = document.createTextNode("body, html {height: 100%;margin: 0;overflow: hidden;}.fullScreen {z-index:999;width: 100%;height: 100% !important;position: fixed;top: 0;left: 0;}.hide{display:none;}.height-auto{height: auto !important}");
+	var style_node = document.createTextNode("body, html {height: 100%;margin: 0;overflow: hidden;}.fullScreen {z-index:999;width: 100%;height: 100% !important;position: fixed;top: 0;left: 0;}.hide{display:none;}.height-auto{height: 100vh !important}");
 	og_style.appendChild(style_node);
 	og_e.appendChild(og_style);
 	//Button
@@ -41,7 +41,7 @@ if(iWidth < 786) {
 	//toggleFullScreen
 	var toggleFullScreen = document.createElement("script");
 	toggleFullScreen.setAttribute("type", "text/javascript");
-	var toggleFullScreen_node = document.createTextNode("function toggleFullScreen(){var e=document.body;document.getElementById('body_wrapper').classList.toggle('fullScreen'),document.getElementById('og-iframe-button').classList.toggle('hide'),document.getElementById('og_overlay').classList.toggle('hide'),document.getElementById('body_wrapper').classList.contains('fullScreen')?document.getElementById('body_wrapper').appendChild(document.getElementById('og-iframe')):document.getElementById('og-iframe-container').appendChild(document.getElementById('og-iframe')),void 0!==document.fullScreenElement&&null===document.fullScreenElement||void 0!==document.msFullscreenElement&&null===document.msFullscreenElement||void 0!==document.mozFullScreen&&!document.mozFullScreen||void 0!==document.webkitIsFullScreen&&!document.webkitIsFullScreen?e.requestFullScreen?e.requestFullScreen():e.mozRequestFullScreen?e.mozRequestFullScreen():e.webkitRequestFullScreen?e.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT):e.msRequestFullscreen&&e.msRequestFullscreen():document.cancelFullScreen?document.cancelFullScreen():document.mozCancelFullScreen?document.mozCancelFullScreen():document.webkitCancelFullScreen?document.webkitCancelFullScreen():document.msExitFullscreen&&document.msExitFullscreen()}");
+	var toggleFullScreen_node = document.createTextNode("function toggleFullScreen(){var e=document.body;document.getElementById('body_wrapper').classList.toggle('fullScreen'),document.getElementById(\"og-iframe\").classList.toggle('height-auto'),document.getElementById('og-iframe-button').classList.toggle('hide'),document.getElementById('og_overlay').classList.toggle('hide'),document.getElementById('body_wrapper').classList.contains('fullScreen')?document.getElementById('body_wrapper').appendChild(document.getElementById('og-iframe')):document.getElementById('og-iframe-container').appendChild(document.getElementById('og-iframe')),void 0!==document.fullScreenElement&&null===document.fullScreenElement||void 0!==document.msFullscreenElement&&null===document.msFullscreenElement||void 0!==document.mozFullScreen&&!document.mozFullScreen||void 0!==document.webkitIsFullScreen&&!document.webkitIsFullScreen?e.requestFullScreen?e.requestFullScreen():e.mozRequestFullScreen?e.mozRequestFullScreen():e.webkitRequestFullScreen?e.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT):e.msRequestFullscreen&&e.msRequestFullscreen():document.cancelFullScreen?document.cancelFullScreen():document.mozCancelFullScreen?document.mozCancelFullScreen():document.webkitCancelFullScreen?document.webkitCancelFullScreen():document.msExitFullscreen&&document.msExitFullscreen()}");
 	toggleFullScreen.appendChild(toggleFullScreen_node);
 	og_e.appendChild(toggleFullScreen);
 	//Overlay
