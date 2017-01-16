fea2d9000285dbc7e85b2fb4075244cb4ed302a5
Himanshu Singh <himanshu.singh@venturepact.com>
2017-01-16 20:14:00 +0530

    Change testing

--- ebookhandler.php
+++ ebookhandler.php
@@ -1,3 +1,15 @@
 <?php
 
-var_dump($_POST);
+require __DIR__."vendor/mailerlite-php-sdk/src/Mailerlite.php";
+
+$groupsApi = (new MailerLite("07c23594acf5764492d5ecae362ff0af"))->groups();
+
+$subscriber = [
+    'email' => $_POST['email'],
+    'fields' => [
+        'name' => $_POST['name'],
+    ]
+];
+
+$response = $groupsApi->addSubscriber(4265413, $subscriber);
+return $response;
