e09eab530830f8f474cd4cf5ed519be410cdf825
Naman Kareer <naman.kareer@venturepact.com>
2017-03-07 11:12:09 +0530

    checking

--- js/loader/iloader.js
+++ js/loader/iloader.js
@@ -604,60 +604,67 @@
         };
     a.jQuery && E(jQuery), "function" == typeof define && define.amd ? define([], D) : "object" == typeof module && "object" == typeof module.exports ? module.exports = D() : a.iFrameResize = a.iFrameResize || D()
 }(window || {});
-console.log('In Script');
-var og_e = document.getElementById('og-iframe-container');
-og_e.setAttribute("style", "line-height: 0;");
-var og_u = og_e.getAttribute('data-url');
-var og_w = og_e.getAttribute('data-width');
-var og_b = document.getElementsByTagName("body")[0];
-og_b.setAttribute("style", "overflow:scroll !important;height: 100%;-webkit-overflow-scrolling: touch !important;");
-var og_js = document.createElement("script");
-og_e.parentElement.setAttribute("style", "width:100% !important;");
-og_js.setAttribute("type", "text/javascript");
-og_js.setAttribute("src", "https://outgrow.co/js/loader/resizer.js");
-og_e.appendChild(og_js);
-var og_iFrame = document.createElement("iframe");
-og_iFrame.setAttribute("id", "og-iframe");
-og_iFrame.setAttribute("style", "border:none;");
-og_iFrame.setAttribute("src", og_u);
-og_iFrame.setAttribute("width", og_w);
-og_iFrame.setAttribute("scrolling", "auto");
-var iWidth = window.innerWidth;
-if (iWidth < 786) {
-    var og_style = document.createElement("style");
-    var style_node = document.createTextNode("body, html {height: 100%;margin: 0;overflow: hidden;}.fullScreen {z-index:9990;width: 100%;height: 100% !important;position: fixed;top: 0;left: 0;}.hide{display:none;}.height-auto{height: 92vh !important; margin-bottom:-10px;}");
-    og_style.appendChild(style_node);
-    og_e.appendChild(og_style);
-    var og_button = document.createElement("a");
-    og_button.setAttribute("href", "javascript:void(0);");
-    og_button.setAttribute("id", "og-iframe-button");
-    og_button.setAttribute("class", "og-iframe-button hide");
-    og_button.setAttribute("onclick", "toggleFullScreen()");
-    og_button.setAttribute("style", "position: absolute;right: 2%;top: 3%;color: black;text-decoration: none;z-index:9999;");
-    var button_node = document.createTextNode("");
-    og_button.appendChild(button_node);
-    og_b.appendChild(og_button);
-    var og_close = document.createElement("img");
-    og_close.setAttribute("src", "https://cdn.filestackcontent.com/HuaIq7oFQwWyS5Fjni9e");
-    og_button.appendChild(og_close);
-    var toggleFullScreen = document.createElement("script");
-    toggleFullScreen.setAttribute("type", "text/javascript");
-    var toggleFullScreen_node = document.createTextNode("function toggleFullScreen(){var e=document.body;document.getElementById('body_wrapper').classList.toggle('fullScreen'),document.getElementById(\"og-iframe\").classList.toggle('height-auto'),document.getElementById('og-iframe-button').classList.toggle('hide'),document.getElementById('og_overlay').classList.toggle('hide'),document.getElementById('body_wrapper').classList.contains('fullScreen')?document.getElementById('body_wrapper').appendChild(document.getElementById('og-iframe')):document.getElementById('og-iframe-container').appendChild(document.getElementById('og-iframe')),void 0!==document.fullScreenElement&&null===document.fullScreenElement||void 0!==document.msFullscreenElement&&null===document.msFullscreenElement||void 0!==document.mozFullScreen&&!document.mozFullScreen||void 0!==document.webkitIsFullScreen&&!document.webkitIsFullScreen?e.requestFullScreen?e.requestFullScreen():e.mozRequestFullScreen?e.mozRequestFullScreen():e.webkitRequestFullScreen?e.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT):e.msRequestFullscreen&&e.msRequestFullscreen():document.cancelFullScreen?document.cancelFullScreen():document.mozCancelFullScreen?document.mozCancelFullScreen():document.webkitCancelFullScreen?document.webkitCancelFullScreen():document.msExitFullscreen&&document.msExitFullscreen()}");
-    toggleFullScreen.appendChild(toggleFullScreen_node);
-    og_e.appendChild(toggleFullScreen);
-    var og_overlay = document.createElement("div");
-    og_overlay.setAttribute("id", "og_overlay");
-    og_overlay.setAttribute("style", "position: absolute;width: 70%;height: 85%;top: 5%;left: 15%;");
-    og_overlay.setAttribute("onclick", "toggleFullScreen()");
-    og_e.appendChild(og_overlay);
-    var body_wrapper = document.createElement("div");
-    body_wrapper.setAttribute("id", "body_wrapper");
-    body_wrapper.setAttribute("style", "overflow: scroll !important;-webkit-overflow-scrolling:touch !important;-webkit-transform: translateZ(0px);-webkit-transform: translate3d(0,0,0);");
-    og_b.appendChild(body_wrapper)
+var og_check	=	0;
+var og_e 		= document.getElementById('og-iframe-container');
+if(og_e.hasAttribute("data-check")) {
+	og_check	=	1;
+} else {
+	og_e.setAttribute("data-check", "1");
 }
-var ogOnLoadHandler = document.createElement("script");
-ogOnLoadHandler.setAttribute("type", "text/javascript");
-var node = document.createTextNode("var screenWidth = screen.width; var screenHeight = screen.height; var aspectRatio = screenWidth/screenHeight;  var width = document.getElementById(\"og-iframe\").clientWidth; var height = width/aspectRatio; if(iWidth < 786){document.getElementById(\"og-iframe\").style.minHeight = height+'px';}else{var iframes = iFrameResize({log:false,autoResize:true,enablePublicMethods: true,minHeight:height})}");
-ogOnLoadHandler.appendChild(node);
-og_e.appendChild(og_iFrame);
-og_e.appendChild(ogOnLoadHandler);
+if(og_check == 0) {
+	og_e.setAttribute("style", "line-height: 0;");
+	var og_u = og_e.getAttribute('data-url');
+	var og_w = og_e.getAttribute('data-width');
+	var og_b = document.getElementsByTagName("body")[0];
+	og_b.setAttribute("style", "overflow:scroll !important;height: 100%;-webkit-overflow-scrolling: touch !important;");
+	var og_js = document.createElement("script");
+	og_e.parentElement.setAttribute("style", "width:100% !important;");
+	og_js.setAttribute("type", "text/javascript");
+	og_js.setAttribute("src", "https://outgrow.co/js/loader/resizer.js");
+	og_e.appendChild(og_js);
+	var og_iFrame = document.createElement("iframe");
+	og_iFrame.setAttribute("id", "og-iframe");
+	og_iFrame.setAttribute("style", "border:none;");
+	og_iFrame.setAttribute("src", og_u);
+	og_iFrame.setAttribute("width", og_w);
+	og_iFrame.setAttribute("scrolling", "auto");
+	var iWidth = window.innerWidth;
+	if (iWidth < 786) {
+		var og_style = document.createElement("style");
+		var style_node = document.createTextNode("body, html {height: 100%;margin: 0;overflow: hidden;}.fullScreen {z-index:9990;width: 100%;height: 100% !important;position: fixed;top: 0;left: 0;}.hide{display:none;}.height-auto{height: 92vh !important; margin-bottom:-10px;}");
+		og_style.appendChild(style_node);
+		og_e.appendChild(og_style);
+		var og_button = document.createElement("a");
+		og_button.setAttribute("href", "javascript:void(0);");
+		og_button.setAttribute("id", "og-iframe-button");
+		og_button.setAttribute("class", "og-iframe-button hide");
+		og_button.setAttribute("onclick", "toggleFullScreen()");
+		og_button.setAttribute("style", "position: absolute;right: 2%;top: 3%;color: black;text-decoration: none;z-index:9999;");
+		var button_node = document.createTextNode("");
+		og_button.appendChild(button_node);
+		og_b.appendChild(og_button);
+		var og_close = document.createElement("img");
+		og_close.setAttribute("src", "https://cdn.filestackcontent.com/HuaIq7oFQwWyS5Fjni9e");
+		og_button.appendChild(og_close);
+		var toggleFullScreen = document.createElement("script");
+		toggleFullScreen.setAttribute("type", "text/javascript");
+		var toggleFullScreen_node = document.createTextNode("function toggleFullScreen(){var e=document.body;document.getElementById('body_wrapper').classList.toggle('fullScreen'),document.getElementById(\"og-iframe\").classList.toggle('height-auto'),document.getElementById('og-iframe-button').classList.toggle('hide'),document.getElementById('og_overlay').classList.toggle('hide'),document.getElementById('body_wrapper').classList.contains('fullScreen')?document.getElementById('body_wrapper').appendChild(document.getElementById('og-iframe')):document.getElementById('og-iframe-container').appendChild(document.getElementById('og-iframe')),void 0!==document.fullScreenElement&&null===document.fullScreenElement||void 0!==document.msFullscreenElement&&null===document.msFullscreenElement||void 0!==document.mozFullScreen&&!document.mozFullScreen||void 0!==document.webkitIsFullScreen&&!document.webkitIsFullScreen?e.requestFullScreen?e.requestFullScreen():e.mozRequestFullScreen?e.mozRequestFullScreen():e.webkitRequestFullScreen?e.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT):e.msRequestFullscreen&&e.msRequestFullscreen():document.cancelFullScreen?document.cancelFullScreen():document.mozCancelFullScreen?document.mozCancelFullScreen():document.webkitCancelFullScreen?document.webkitCancelFullScreen():document.msExitFullscreen&&document.msExitFullscreen()}");
+		toggleFullScreen.appendChild(toggleFullScreen_node);
+		og_e.appendChild(toggleFullScreen);
+		var og_overlay = document.createElement("div");
+		og_overlay.setAttribute("id", "og_overlay");
+		og_overlay.setAttribute("style", "position: absolute;width: 70%;height: 85%;top: 5%;left: 15%;");
+		og_overlay.setAttribute("onclick", "toggleFullScreen()");
+		og_e.appendChild(og_overlay);
+		var body_wrapper = document.createElement("div");
+		body_wrapper.setAttribute("id", "body_wrapper");
+		body_wrapper.setAttribute("style", "overflow: scroll !important;-webkit-overflow-scrolling:touch !important;-webkit-transform: translateZ(0px);-webkit-transform: translate3d(0,0,0);");
+		og_b.appendChild(body_wrapper)
+	}
+	var ogOnLoadHandler = document.createElement("script");
+	ogOnLoadHandler.setAttribute("type", "text/javascript");
+	var node = document.createTextNode("var screenWidth = screen.width; var screenHeight = screen.height; var aspectRatio = screenWidth/screenHeight;  var width = document.getElementById(\"og-iframe\").clientWidth; var height = width/aspectRatio; if(iWidth < 786){document.getElementById(\"og-iframe\").style.minHeight = height+'px';}else{var iframes = iFrameResize({log:false,autoResize:true,enablePublicMethods: true,minHeight:height})}");
+	ogOnLoadHandler.appendChild(node);
+	og_e.appendChild(og_iFrame);
+	og_e.appendChild(ogOnLoadHandler);
+}
