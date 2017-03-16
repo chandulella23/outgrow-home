4fdaf1e8d1d98cc9a103878bf26f9e872338dfe6
Bhawna Thakur <bhawna.thakur@venturepact.com>
2017-03-16 14:11:03 +0530

    height

--- js/iloader.js
+++ js/iloader.js
@@ -637,7 +637,7 @@ if (og_check == 0) {
 			var style_node = document.createTextNode("body, html {height: 100%;margin: 0;}.fullScreen {z-index:9990;width: 100%;height: 100% !important;position: fixed;top: 0;left: 0;}.hide{display:none;}.height-auto{height: 100vh;}.ovr-hid{overflow: hidden !important;}");
 		}
         else{
-			var style_node = document.createTextNode("body, html {height: 100%;margin: 0;}.fullScreen {z-index:9990;width: 100%;height: 100% !important;position: fixed;top: 0;left: 0;}.hide{display:none;}.height-auto{height: 100%;}.ovr-hid{overflow: hidden !important;}");
+			var style_node = document.createTextNode("body, html {height: 100%;margin: 0;}.fullScreen {z-index:9990;width: 100%;height: 100% !important;position: fixed;top: 0;left: 0;}.hide{display:none;}.height-auto{height: 100%; margin-bottom: -10px;}.ovr-hid{overflow: hidden !important;}");
 		}
         og_style.appendChild(style_node);
         og_e.appendChild(og_style);
