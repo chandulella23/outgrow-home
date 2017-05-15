70a4f49448388109385dd583dc9c2db2cf8b5305
Himanshu Singh <himanshuakatalli@live.com>
2017-05-15 17:24:13 +0530

    Source commit

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ git add . && git commit -am "Source commit"
 
 git push origin jigsawhome
 
-git add build_ogco && git commit -am "Build for production"
+jigsaw build ogco && git add build_ogco && git commit -am "Build for production"
 
 git subtree push --prefix build_ogco origin gh-pages
