f2030e507b8a2eb5d5082cca41664f3f126f4c57
Himanshu Singh <himanshuakatalli@live.com>
2017-05-22 16:18:17 +0530

    New page added

---
+++ source/newpage.blade.php
@@ -0,0 +1,23 @@
+@extends('_layouts.master')
+
+@section('title', '')
+
+@section('metaDescription')
+@endsection
+
+@section('inlinescripts')
+@endsection
+
+@section('css')
+@endsection
+
+@section('pageClass', '')
+
+@section('pageId', '')
+
+@section('content')
+@endsection
+
+@section('pageScripts')
+	
+@endsection
