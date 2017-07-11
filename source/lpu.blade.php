6acd2d3493cc4d2bd335bf74ccba82ed3843bb3f
Arti <arti.thind@venturepact.com>
2017-07-11 15:20:19 +0530

    removed height of video

--- source/lpu.blade.php
+++ source/lpu.blade.php
@@ -128,10 +128,10 @@
 					window.location.replace('#bfrVid')
 					document.getElementsByClassName("video-img-inner-res")[0].innerHTML = "<div class='embed-responsive embed-responsive-16by9'><iframe class='outgrow-video' src='https://www.youtube.com/embed/PmN_MY5kNrE?vq=hd720&amp;rel=0&amp;controls=0&amp;showinfo=0;autoplay=1&amp;iv_load_policy=3' frameborder='0' allowfullscreen></iframe></div>";
 					var xDiv = document.getElementById('video-main-rs');
-					if (xDiv.style.height == '')
-						xDiv.style.height = '460px';
-					else
-						xDiv.style.height = '';
+					// if (xDiv.style.height == '')
+					// 	xDiv.style.height = '460px';
+					// else
+					// 	xDiv.style.height = '';
 				}
 				function changeHeightWeb1 () {
 					console.log('Tab');
