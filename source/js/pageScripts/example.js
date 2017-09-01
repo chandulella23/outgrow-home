1abaf45199494484ab1eaeeaf9313d1d8db8542a
Himanshu Singh <himanshuakatalli@live.com>
2017-09-01 15:48:13 +0530

    Removed resizer call for every display

--- source/js/pageScripts/example.js
+++ source/js/pageScripts/example.js
@@ -5,17 +5,9 @@ window.runTimeout = function() {
 }
 
 window.display = function (url) {
-	//console.log('Display called', url)
-	//jQuery('.hrefTarget').empty().append('<iframe id="og-iframe" src="'+url+'"></iframe>');
-	document.getElementById('og-iframe').src = url;
-	// var iframes = iFrameResize({
-    //     log: false,
-    //     autoResize: true,
-    //     enablePublicMethods: true,
-    //     checkOrigin: false,
-    // },'#og-iframe');
+    document.getElementById('og-iframe').src = url;
     jQuery('#og-iframe').addClass('iframeHeight')
-    calculateMinHeight();
+	calculateMinHeight();
 }
 
 jQuery(document).ready(function () {
@@ -26,7 +18,9 @@ jQuery(document).ready(function () {
         enablePublicMethods: true,
         checkOrigin: false,
     },'#og-iframe');
+    
     calculateMinHeight();
+
     jQuery('.calc-links a').on('click',function(){
 		jQuery('a').removeClass('active');
 		jQuery(this).addClass('active');
