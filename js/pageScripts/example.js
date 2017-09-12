7c3819757f767e9b6fd09a4a7db52288216997db
Himanshu Singh <himanshuakatalli@live.com>
2017-09-12 12:21:25 +0530

    Build for production-stable

---
+++ js/pageScripts/example.js
@@ -0,0 +1,37 @@
+window.runTimeout = function() {
+    setTimeout(function () {
+        jQuery('.content-loader').addClass('hide');   
+    }, 5000);
+}
+
+window.display = function (url) {
+    document.getElementById('og-iframe').src = url;
+    jQuery('#og-iframe').addClass('iframeHeight')
+    calculateMinHeight();
+    
+    setTimeout(calculateMinHeight, 2000);
+}
+
+jQuery(document).ready(function () {
+	calculateMinHeight();
+	var iframes = iFrameResize({
+        log: false,
+        autoResize: true,
+        enablePublicMethods: true,
+        checkOrigin: false,
+    },'#og-iframe');
+
+    jQuery('.calc-links a').on('click',function(){
+		jQuery('a').removeClass('active');
+		jQuery(this).addClass('active');
+	});
+    jQuery('.og-iframe-res').each(function () {
+        console.log('Examples og');
+        if (jQuery(this).attr('data-calc')) {
+            console.log('Examples og if');
+            jQuery(this).attr('src', jQuery(this).attr('data-calc'));
+            jQuery(this).attr('data-calc', '');
+        }
+    })
+    runTimeout();
+})
