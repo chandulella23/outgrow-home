a234ab19d2ae20b4c3a7d488c0ae94cd3453b882
Arti <arti.thind@venturepact.com>
2018-01-29 14:57:26 +0530

    top ranked images & text changes

--- source/index.blade.php
+++ source/index.blade.php
@@ -1526,31 +1526,38 @@
 		<div class="swiper-container-ranking rs-hide">
 			<div class="swiper-wrapper">
 				<div class="swiper-slide" data-history="1">
-					<img data-u="image" src="{{ $page->baseUrl }}/images/ranking-swiper-img1.jpg" />
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_1.jpg" />
 					<div class="swiper-content">
-						<h3>Mste natus error sit voluptatem accusantium doloremque laudantium</h3>
-						<span>Lorem ipsum dolor in reprehenderit in voluptate velit esse cillum dolore eu</span>
+						<!-- <h3>Outgrow has the highest overall Usability Score among content marketing software</h3> -->
+						<span>Outgrow has the highest overall Usability Score among content marketing software</span> 
 					</div>
 				</div>
 				<div class="swiper-slide" data-history="Slide 2">
-					<img data-u="image" src="{{ $page->baseUrl }}/images/ranking-swiper-img1.jpg" />
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_2.jpg" />
 					<div class="swiper-content">
-						<h3>Mste natus error sit voluptatem accusantium doloremque laudantium</h3>
-						<span>Lorem ipsum dolor in reprehenderit in voluptate velit esse cillum dolore eu</span>
+						<!-- <h3>Outgrow is the highest rated content marketing software among G2 Crowd reviewers with a Satisfaction Score of 100%</h3> -->
+						<span>Outgrow is the highest rated content marketing software among G2 Crowd reviewers with a Satisfaction Score of 100%</span> 
 					</div>
 				</div>
 				<div class="swiper-slide" data-history="Slide 3">
-					<img data-u="image" src="{{ $page->baseUrl }}/images/ranking-swiper-img1.jpg" />
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_3.jpg" />
 					<div class="swiper-content">
-						<h3>Mste natus error sit voluptatem accusantium doloremque laudantium</h3>
-						<span>Lorem ipsum dolor in reprehenderit in voluptate velit esse cillum dolore eu</span>
+						<!-- <h3>Outgrow listed as the most ROI-friendly Content Marketing Software</h3> -->
+						<span>Outgrow listed as the most ROI-friendly Content Marketing Software</span> 
 					</div>
 				</div>
 				<div class="swiper-slide" data-history="Slide 4">
-					<img data-u="image" src="{{ $page->baseUrl }}/images/ranking-swiper-img1.jpg" />
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_4.jpg" />
 					<div class="swiper-content">
-						<h3>Mste natus error sit voluptatem accusantium doloremque laudantium</h3>
-						<span>Lorem ipsum dolor in reprehenderit in voluptate velit esse cillum dolore eu</span>
+						<!-- <h3>Outgrow is listed as the number 1 content marketing software for small business</h3> -->
+						<span>Outgrow is listed as the number 1 content marketing software for small business</span> 
+					</div>
+				</div>
+				<div class="swiper-slide" data-history="Slide 5">
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_5.jpg" />
+					<div class="swiper-content">
+						<!-- <h3>Outgrow ranks 1st when it comes to Ease-of-Implementation among content marketing software</h3> -->
+						<span>Outgrow ranks 1st when it comes to Ease-of-Implementation among content marketing software</span> 
 					</div>
 				</div>
 			</div>
@@ -1581,31 +1588,38 @@
 		<div class="swiper-container-ranking-res rs-show">
 			<div class="swiper-wrapper">
 				<div class="swiper-slide" data-history="1">
-					<img data-u="image" src="{{ $page->baseUrl }}/images/ranking-swiper-img1.jpg" />
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_1.jpg" />
 					<div class="swiper-content">
-						<h3>Mste natus error sit voluptatem accusantium doloremque laudantium</h3>
-						<span>Lorem ipsum dolor in reprehenderit in voluptate velit esse cillum dolore eu</span>
+						<!-- <h3>Outgrow has the highest overall Usability Score among content marketing software</h3> -->
+						<span>Outgrow has the highest overall Usability Score among content marketing software</span> 
 					</div>
 				</div>
 				<div class="swiper-slide" data-history="Slide 2">
-					<img data-u="image" src="{{ $page->baseUrl }}/images/ranking-swiper-img1.jpg" />
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_2.jpg" />
 					<div class="swiper-content">
-						<h3>Mste natus error sit voluptatem accusantium doloremque laudantium</h3>
-						<span>Lorem ipsum dolor in reprehenderit in voluptate velit esse cillum dolore eu</span>
+						<!-- <h3>Outgrow is the highest rated content marketing software among G2 Crowd reviewers with a Satisfaction Score of 100%</h3> -->
+						<span>Outgrow is the highest rated content marketing software among G2 Crowd reviewers with a Satisfaction Score of 100%</span> 
 					</div>
 				</div>
 				<div class="swiper-slide" data-history="Slide 3">
-					<img data-u="image" src="{{ $page->baseUrl }}/images/ranking-swiper-img1.jpg" />
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_3.jpg" />
 					<div class="swiper-content">
-						<h3>Mste natus error sit voluptatem accusantium doloremque laudantium</h3>
-						<span>Lorem ipsum dolor in reprehenderit in voluptate velit esse cillum dolore eu</span>
+						<!-- <h3>Outgrow listed as the most ROI-friendly Content Marketing Software</h3> -->
+						<span>Outgrow listed as the most ROI-friendly Content Marketing Software</span> 
 					</div>
 				</div>
 				<div class="swiper-slide" data-history="Slide 4">
-					<img data-u="image" src="{{ $page->baseUrl }}/images/ranking-swiper-img1.jpg" />
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_4.jpg" />
 					<div class="swiper-content">
-						<h3>Mste natus error sit voluptatem accusantium doloremque laudantium</h3>
-						<span>Lorem ipsum dolor in reprehenderit in voluptate velit esse cillum dolore eu</span>
+						<!-- <h3>Outgrow is listed as the number 1 content marketing software for small business</h3> -->
+						<span>Outgrow is listed as the number 1 content marketing software for small business</span> 
+					</div>
+				</div>
+				<div class="swiper-slide" data-history="Slide 5">
+					<img data-u="image" src="{{ $page->baseUrl }}/images/top_ranked_img_5.jpg" />
+					<div class="swiper-content">
+						<!-- <h3>Outgrow ranks 1st when it comes to Ease-of-Implementation among content marketing software</h3> -->
+						<span>Outgrow ranks 1st when it comes to Ease-of-Implementation among content marketing software</span> 
 					</div>
 				</div>
 			</div>
