1c58a8633bad40ecc3b6f5620ae4f349877c2f66
Arti <arti.thind@venturepact.com>
2018-02-13 16:26:07 +0530

    text changes for freelancer plan

--- source/pricing.blade.php
+++ source/pricing.blade.php
@@ -190,7 +190,7 @@
                             </th>
                             <td class="expand-child">
                                 <span class="quater"></span>Basic Templates & Layout
-                                <span class="detail">Only the classic Chicago and Londoner layout and templates.</span>
+                                <span class="detail">Only the Classic "Chicago" layout and templates.</span>
                             </td>
                             <td class="expand-child active-bg">
                                 <span class="half"></span>All Templates & Layouts
@@ -520,7 +520,7 @@
                                         Templates and Layouts
                                     </span>
                                     <span class="feature-subtitle"><span class="half"></span>Basic Templates & Layout </span>
-                                    <span class="detail">Only the classic Chicago and Londoner layout and templates. </span>
+                                    <span class="detail">Only the Classic "Chicago" layout and templates. </span>
                                 </li>
                                 <li class="expand-rs">
                                     <span class="feature-title">
