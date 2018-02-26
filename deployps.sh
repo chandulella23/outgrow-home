48c72cca91864510619e9b75441a77ee20f84d14
Himanshu Singh <himanshu.singh@venturepact.com>
2018-02-26 17:04:48 +0530

    changes

--- deployps.sh
+++ deployps.sh
@@ -7,5 +7,5 @@ git pull origin production-stable-source && git commit -am "Pulled changes"
 git push origin production-stable-source
 
 jigsaw build ogps && git add build_ogps && git commit -am "Build for production-stable"
-
+jigsaw serve ogps
 git subtree push --prefix build_ogps origin production-stable
