c8bb2c752ee47725379a0925738bc4eb3b0af0c7
Arti <arti.thind@venturepact.com>
2018-03-06 16:27:19 +0530

    added function for premade calc

--- source/js/pageScripts/index.js
+++ source/js/pageScripts/index.js
@@ -77,6 +77,105 @@ window.display = function (url) {
     setTimeout(calculateMinHeight, 2000);
 }
 
+window.calcs = [
+    {
+        'id': 'calc-auto-loan',
+        filters: ['filter-all', 'filter-auto'],
+        calcLink: 'https://premade.outgrow.us/Auto-Loan-Calc',
+        gif: 'https://dzvexx2x036l1.cloudfront.net/calc_images/auto+loan+calculator.gif',
+        name: 'AUTO LOAN CALCULATOR',
+        layout: 'The Londoner'
+    },
+    {
+        'id': 'calc-early-payment',
+        filters: ['filter-all', 'filter-auto'],
+        calcLink: 'https://premade.outgrow.us/Early-Payment-Calculator',
+        gif: 'https://dzvexx2x036l1.cloudfront.net/calc_images/early+payment+calculator.gif',
+        name: 'EARLY PAYMENT CALCULATOR',
+        layout: 'The Tokyo'
+    },
+    {
+        'id': 'calc-downpayment',
+        filters: ['filter-all', 'filter-auto'],
+        calcLink: 'https://premade.outgrow.us/Downpayment-Calculator',
+        gif: 'https://dzvexx2x036l1.cloudfront.net/calc_images/downpayment+calculator.gif',
+        name: 'HOW MUCH SHOULD I PUT DOWN?',
+        layout: 'The Londoner'
+    },
+    {
+        'id': 'calc-student-budget',
+        filters: ['filter-all', 'filter-education'],
+        calcLink: 'https://premade.outgrow.us/student-budget-calculator',
+        gif: 'https://dzvexx2x036l1.cloudfront.net/calc_images/calculate+your+student+budget.gif',
+        name: 'STUDENT BUDGET CALCULATOR',
+        layout: 'The Madrid'
+    },
+    {
+        'id': 'calc-harward-score',
+        filters: ['filter-all', 'filter-education'],
+        calcLink: 'https://premade.outgrow.us/Harvard-Minimum-Score-Calculator',
+        gif: 'https://dzvexx2x036l1.cloudfront.net/calc_images/chances+of+getting+into+harvard.gif',
+        name: 'What is a good enough score to get into Harvard with your background?',
+        layout: 'The Greek'
+    },
+    {
+        'id': 'calc-home-budget',
+        filters: ['filter-all', 'filter-finance'],
+        calcLink: 'https://premade.outgrow.us/Home-Budget-Calculator',
+        gif: 'https://dzvexx2x036l1.cloudfront.net/calc_images/calculate+your+monthly+home+budget.gif',
+        name: 'HOME BUDGET CALCULATOR',
+        layout: 'The Tokyo'
+    },
+    {
+        'id': 'calc-weight-loss',
+        filters: ['filter-all', 'filter-health'],
+        calcLink: 'https://premade.outgrow.us/weightloss-calc',
+        gif: 'https://dzvexx2x036l1.cloudfront.net/calc_images/Weightloss+Calc.gif',
+        name: 'WEIGHT LOSS CALCULATOR',
+        layout: 'The Greek'
+    },
+    {
+        'id': "calc-trump-plan",
+        filters: ['filter-all', 'filter-publishing'],
+        calcLink: 'https://premade.outgrow.us/How-will-Trump-tax-plan-affect-you',
+        gif: 'https://dzvexx2x036l1.cloudfront.net/calc_images/how+will+trumps+tax+plan+affect+you.gif',
+        name: 'HOW WILL TRUMP\'S TAX PLAN AFFECT YOU?',
+        layout: 'The Madrid'
+    }
+];
+
+window.markAsActive = function (calcId) {
+    var currentActiveCalc = document.querySelector('#gallery-content-center li.active');
+    currentActiveCalc.classList.remove('active');
+
+    var tobeActiveCalc = document.querySelector('#' + calcId);
+    tobeActiveCalc.classList.add('active');
+
+    toBeActiveCalc = window.calcs.find(function (calc) {
+        return calc.id === calcId;
+    });
+    
+    var premadeGif = document.getElementById('premade-gif');
+    var premadePreview = document.getElementById('premade-preview-link');
+    var premadeName = document.getElementById('premade-calc-name');
+    var premadeLayout = document.getElementById('premade-calc-layout')
+
+    premadeGif.src = toBeActiveCalc.gif;
+    premadePreview.href = toBeActiveCalc.calcLink;
+    premadeName.innerHTML = '<i class="material-icons">&#xE80E;</i>' + toBeActiveCalc.name;
+    premadeLayout.innerHTML = '<strong>Layout:</strong> ' + toBeActiveCalc.layout;
+};
+
+window.changeActiveCalcCategory = function (id) {
+    var currentActiveCat = document.querySelector('#calc-cats li.active');
+    currentActiveCat.classList.remove('active');
+
+    var tobeActiveCat = document.querySelector('#' + id);
+    tobeActiveCat.parentElement.classList.add('active');
+}
+
+window.shuffleCalcs = function (filterName) {
+
 jQuery.noConflict();
 jQuery.material.init();
 jQuery(document).ready(function() {
