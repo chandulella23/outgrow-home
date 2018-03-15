3e4bd490e0a98ebca39dbf4506bd3184c1fcb349
Arti <arti.thind@venturepact.com>
2018-03-15 11:34:57 +0530

    footer svg logo added

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -89,7 +89,7 @@
 			<div class="footer-copyright">
 				<i class="material-icons">copyright</i>
 				<span>Copyright 2018</span>
-				<img class="scrollimg" data-scroll-img="//dzvexx2x036l1.cloudfront.net/footer-logo-dark.png" src="//dzvexx2x036l1.cloudfront.net/footer-logo-dark.png" alt="Footer logo" />
+				<img class="scrollimg" data-scroll-img="images/outgrow_logo_grey.svg" src="images/outgrow_logo_grey.svg" alt="Footer logo" />
 				<a style="display:none;" href="https://trustlogo.com/install/index5.html?certificatename=Comodo-SSL-Wildcard" target="_blank"><img class="scrollimg footer-img-2 rs-hide-mb" data-scroll-img="//dzvexx2x036l1.cloudfront.net/comodo_secure.png" src="" alt="comodo_secure image" /></a>
 			</div>
 		</div>
