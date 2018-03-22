1405d6fff234deec0b156d9f30ef9228da694789
Arti <arti.thind@venturepact.com>
2018-03-22 17:13:54 +0530

    footer fixes with updated links & images

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,4 +1,4 @@
-<section class="section section-14" id="footer">
+<section class="section section-14 hide" id="footer">
 	<div class="container">
 		<div class="col-xs-12 col-sm-12 np">
 			<div class="col-xs-12 col-sm-2 footer-link mk-animate-element fade-in">
@@ -91,6 +91,123 @@
 				<span>Copyright 2018</span>
 				<img class="scrollimg" data-scroll-img="https://dzvexx2x036l1.cloudfront.net/outgrow_logo_grey.svg" src="https://dzvexx2x036l1.cloudfront.net/outgrow_logo_grey.svg" alt="Footer logo" />
 				<a style="display:none;" href="https://trustlogo.com/install/index5.html?certificatename=Comodo-SSL-Wildcard" target="_blank"><img class="scrollimg footer-img-2 rs-hide-mb" data-scroll-img="//dzvexx2x036l1.cloudfront.net/comodo_secure.png" src="" alt="comodo_secure image" /></a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.css">
+<section class="section section-14" id="footer">
+	<div class="container">
+		<div class="col-xs-12 col-sm-12 np">
+			<div class="badge-g2crowd">
+				<img class="g2crowd"  src="{{ $page->baseUrl }}/images/badge_g2crowd.svg">
+			</div> 
+			<div class="col-xs-12 col-sm-2 footer-link mk-animate-element fade-in">
+				<h4>Menu</h4>
+				<ul>
+					<li><a href="{{ $page->baseUrl }}/why-interactive" onclick="callGA(\'WHYF\')" class="params">Why Interactive Content?</a></li>
+					<li><a href="{{ $page->baseUrl }}/features" onclick="callGA(\'FEATURESF\')" class="params">Features </a></li>
+					<li><a href="{{ $page->baseUrl }}/examples" onclick="callGA(\'USECASEF\')" class="params">Examples</a></li>
+					<!-- <li><a href="{{ $page->baseUrl }}/idea-generation" onclick="callGA(\'IDEAGEN\')" class="params" target="_blank">Idea Generator</a></li> -->
+					<li><a href="{{ $page->baseUrl }}/pricing" onclick="callGA(\'PRICINGF\')" class="params">Pricing</a></li>
+
+					<!-- <li><a href="http://affiliates.outgrow.co/">Affiliates</a></li> -->
+					<li><a href="http://resources.outgrow.co/" onclick="callGA(\'RESOURCEF\')" class="params">Blog</a></li>
+
+					<li><a href="http://resources.outgrow.co/about" onclick="callGA(\'ABOUTF\')" class="params">About</a></li>
+					<li><a href="javascript:void(0)" target="_top" class="intercom_trigger">Contact us</a></li>
+				</ul>
+			</div>
+			<!-- <div class="col-xs-12 col-sm-5 footer-link mk-animate-element fade-in hide">
+				<h4>Our Live Calculators</h4>
+				<div class="scrollimg footer-img col-xs-6 col-sm-6"><img data-scroll-img="//dzvexx2x036l1.cloudfront.net/footer1.jpg" src="" /></div>
+				<div class="scrollimg footer-img col-xs-6 col-sm-6"><img data-scroll-img="//dzvexx2x036l1.cloudfront.net/footer2.jpg" src="" /></div>
+				<div class="scrollimg footer-img col-xs-6 col-sm-6"><img data-scroll-img="//dzvexx2x036l1.cloudfront.net/footer4.jpg" src="" /></div>
+				<div class="scrollimg footer-img col-xs-6 col-sm-6"><img data-scroll-img="//dzvexx2x036l1.cloudfront.net/footer3.jpg" src="" /></div>
+			</div> -->
+			<div class="col-xs-12 col-sm-2 footer-link mk-animate-element fade-in">
+				<h4 class="footer-img2text col-xs-12">eBooks</h4>
+				<div class="footer-img2 col-xs-6 col-sm-12">
+					<div class="col-xs-12 np"><img class="scrollimg" data-scroll-img="{{ $page->baseUrl }}/images/footer-ebook1.jpg"" src="{{ $page->baseUrl }}/images/footer-ebook1.jpg" /></div>
+					<div class="col-xs-12 np footer-img-text">							
+						<a href="https://get.outgrow.co/interactive-content-marketing-for-digital-agencies">Interactive Content Marketing for Digital Agencies </a>
+					</div>
+				</div>
+
+				<div class="footer-img2 col-xs-6 col-sm-12">
+					<div class="col-xs-12 np"><img class="scrollimg" data-scroll-img="{{ $page->baseUrl }}/images/footer-ebook2.jpg"" src="{{ $page->baseUrl }}/images/footer-ebook2.jpg" /></div>
+					<div class="col-xs-12 np footer-img-text">						
+						<a href="https://get.outgrow.co/guide-to-building-viral-quizzes-and-calculators">Creative's Guide to building Viral Quizzes and Calculators</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-2 footer-link mk-animate-element fade-in">
+				<h4 class="footer-img2text col-xs-12">Tools</h4>
+				<div class="footer-img2 col-xs-6 col-sm-12">
+					<div class="col-xs-12 np"><img class="scrollimg" data-scroll-img="{{ $page->baseUrl }}/images/footer-tools1.jpg"" src="{{ $page->baseUrl }}/images/footer-tools1.jpg" /></div>
+					<div class="col-xs-12 np footer-img-text">							
+						<a href="https://influencers.outgrow.us/roi-of-outgrow">Calculate the ROI of using Outgrow</a>
+					</div>
+				</div>
+				<div class="footer-img2 col-xs-6 col-sm-12">
+					<div class="col-xs-12 np"><img class="scrollimg" data-scroll-img="{{ $page->baseUrl }}/images/footer-tools2.jpg"" src="{{ $page->baseUrl }}/images/footer-tools2.jpg" /></div>
+					<div class="col-xs-12 np footer-img-text">						
+						<a href="https://outgrow.co/idea-generation/">Generate Ideas for Quizzes and Calculators</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-4 footer-link mk-animate-element fade-in">
+				<h4 class="footer-img2text col-xs-12">From our blog</h4>
+				<div class="footer-img2 col-xs-6 col-sm-6">
+					<div class="col-xs-12 np"><img class="scrollimg" data-scroll-img="{{ $page->baseUrl }}/images/footer-blog1.jpg"" src="{{ $page->baseUrl }}/images/footer-blog1.jpg" /></div>
+					<div class="col-xs-12 np footer-img-text">						
+						<a href="https://outgrow.co/blog/top-10-interactive-quizzes-calculators-2017">Highest-performing Interactive Quizzes and Calculators</a>
+					</div>
+				</div>
+				<div class="footer-img2 col-xs-6 col-sm-6">
+					<div class="col-xs-12 np"><img class="scrollimg" data-scroll-img="{{ $page->baseUrl }}/images/footer-blog2.jpg"" src="{{ $page->baseUrl }}/images/footer-blog2.jpg" /></div>
+					<div class="col-xs-12 np footer-img-text">							
+						<a href="https://outgrow.co/blog/interactive-content-engagement-tips">How to Make Quizzes and Calculators Engaging</a>
+					</div>
+				</div>
+				<div class="footer-img2 col-xs-6 col-sm-6">
+					<div class="col-xs-12 np"><img class="scrollimg" data-scroll-img="{{ $page->baseUrl }}/images/footer-blog3.jpg"" src="{{ $page->baseUrl }}/images/footer-blog3.jpg" /></div>
+					<div class="col-xs-12 np footer-img-text">						
+						<a href="https://outgrow.co/blog/get-paid-for-your-pad-interactive-marketing-success-story">Get Paid for your Paid: Interactive Marketing Success Story</a>
+					</div>
+				</div>
+				<div class="footer-img2 col-xs-6 col-sm-6">
+					<div class="col-xs-12 np"><img class="scrollimg" data-scroll-img="{{ $page->baseUrl }}/images/footer-blog4.jpg"" src="{{ $page->baseUrl }}/images/footer-blog4.jpg" /></div>
+					<div class="col-xs-12 np footer-img-text">							
+						<a href="https://outgrow.co/blog/how-an-interactive-calculator-generated-11592-qualified-leads/">How an Interactive Calculator Generated 11,592 Qualified Leads</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-12 np footer-social">
+			<ul>
+				<!-- <li><a href="https://www.facebook.com/OutgrowCo/">Facebook</a></li>
+				<li><a href="https://www.linkedin.com/company/outgrow.co">Linkedin</a></li>
+				<li><a href="https://twitter.com/outgrowco">Twitter</a></li>-->				
+				<li class=""><a href="{{ $page->baseUrl }}/privacy-policy" onclick="callGA(\'PRIVACYF\')" class="params">Privacy Policy</a></li>
+				<li><a href="{{ $page->baseUrl }}/tos" onclick="callGA(\'TERMSF\')" class="params">Terms of Services</a></li>
+				<!-- <li><a href="javascript:void(0)" target="_top" class="intercom_trigger">Contact Us</a></li> -->
+				<li><a href="https://angel.co/venturepact/jobs">Careers</a></li>
+			</ul>
+			<ul>
+				<li class="social-icons"><a href="https://www.facebook.com/OutgrowCo/" target="_blank"><i class="zmdi zmdi-facebook-box"></i></a></li>
+				<li class="social-icons"><a href="https://www.linkedin.com/company/7966190/" target="_blank"><i class="zmdi zmdi-linkedin-box"></i></a></li>
+				<li class="social-icons"><a href="https://twitter.com/outgrowco" target="_blank"><i class="zmdi zmdi-twitter-box"></i></a></li>
+			</ul>
+			<div class="company-address">
+				401 park ave, 10th floor, New York 10016
+			</div>
+			<div class="footer-copyright">
+				<i class="material-icons">copyright</i>
+				<span>Copyright 2018</span>
+				<img class="scrollimg" data-scroll-img="https://dzvexx2x036l1.cloudfront.net/outgrow_logo_grey.svg" src="https://dzvexx2x036l1.cloudfront.net/outgrow_logo_grey.svg" />
+				<a style="display:none;" href="https://trustlogo.com/install/index5.html?certificatename=Comodo-SSL-Wildcard" target="_blank"><img class="scrollimg footer-img-2 rs-hide-mb" data-scroll-img="//dzvexx2x036l1.cloudfront.net/comodo_secure.png" src="" /></a>
 			</div>
 		</div>
 	</div>
