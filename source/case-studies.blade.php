0640be8ee2761349c27bfb2a9cd09bdca66a1c50
Arti <arti.thind@venturepact.com>
2018-04-03 16:42:20 +0530

    Case study: completed sections

--- source/case-studies.blade.php
+++ source/case-studies.blade.php
@@ -95,6 +95,83 @@
 						</div>
 					</div>
 				</section>
+				<section class="section section-companyInfo">
+					<div class="container">
+						<div class="col-md-12 col-sm-12 col-xs-12 np companyInfo-table">
+							<div class="col-md-6 col-sm-6 col-xs-12 np companyInfo-table-cell">
+								<img class="img-shadow" src="{{ $page->baseUrl }}/images/case_study_companyinfo.jpg" alt="TAG Livros Company">
+							</div>
+							<div class="col-md-6 col-sm-6 col-xs-12 np companyInfo-table-cell">
+								<h4>Company</h4>
+								<p>TAG Livros is a book signing club which delivers a literary kit to all its members every month. </p>
+								<h4>Background</h4>
+								<p>Team TAG had been exploring ways to generate more leads and engage them better. However, just like any digital startup, they had a lean budget. Clearly, they needed a lead generation solution that not only had a higher ROI compared to standard inbound techniques, but was also cost effective. </p>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section class="section section-experimenting">
+					<div class="container">
+						<div class="col-md-12 col-sm-12 col-xs-12 np text-center">
+							<div class="section-2-heading">Experimenting With Interactive Content </div>
+							<span class="section-subtitle">The TAG Livros team built an interactive quiz that would tell people about the best selling book from the year they were born - an interesting concept that would encourge people to engage with the quiz! </span>
+							<img class="img-shadow" src="{{ $page->baseUrl }}/images/case_study_experimenting.jpg" alt="Experimenting With Interactive Content">
+						</div>
+					</div>
+				</section>
+				<section class="section section-howOutgrowHelped">
+					<div class="container">
+						<div class="section-2-heading">How Outgrow Helped </div>
+						<div class="col-md-12 col-sm-12 col-xs-12 np section-howOutgrowHelped-inner">
+							<div class="col-md-4 col-sm-4 col-xs-12">
+								<img src="{{ $page->baseUrl }}/images/case_study_howOutgrowHelped1.png" alt="No Coding Required">
+								<h5>No Coding Required </h5>
+								<p>The team built an intutive quiz using Outgrow’s interactive pre-built templates. Thus, they did not have to write a single line of code, which reduced the development cost. </p>
+							</div>
+							<div class="col-md-4 col-sm-4 col-xs-12">
+								<img src="{{ $page->baseUrl }}/images/case_study_howOutgrowHelped2.png" alt="High Completion Rate, More Shares">
+								<h5>High Completion Rate, More Shares </h5>
+								<p>The quiz had a simple landing page, and a single question, which could be answered using a drop down. This format increased their completion rate and their share rate. </p>
+							</div>
+							<div class="col-md-4 col-sm-4 col-xs-12">
+								<img src="{{ $page->baseUrl }}/images/case_study_howOutgrowHelped3.png" alt="More Leads, Improved Conversions">
+								<h5>More Leads, Improved Conversions </h5>
+								<p>The quiz TAG built was interactive, engaged users, and provided instant gratification. Since it was about best-selling books, it was share-worthy. </p>
+							</div>
+						</div>
+						<div class="col-md-12 col-sm-12 col-xs-12 np text-center img-wrapper">
+							<img class="img-shadow" src="{{ $page->baseUrl }}/images/case_study_howOutgrowHelped4.jpg" alt="">
+							<img class="img-shadow" src="{{ $page->baseUrl }}/images/case_study_howOutgrowHelped5.jpg" alt="">
+							<button type="button" class="btn btn-red">Create your First Interactive Experience </button>
+						</div>
+					</div>
+				</section>
+				<section class="section section-companyReviews">
+					<div class="container">
+						<div class="col-md-12 col-sm-12 col-xs-12 np text-center">
+							<blockquote>Outgrow’s well designed templates, allowed us to quickly build and launch experiences without writing a line of code. Their personalization features like logic jump and conditional call to actions helped us personalize the results for our users. </blockquote>
+							<span>Luise Spieweck Fialho <br/>Founder at TAG Livros </span>
+						</div>
+					</div>
+				</section>
+				<section class="section section-results">
+					<div class="container">
+						<div class="section-2-heading">Results </div>
+						<div class="col-md-12 col-sm-12 col-xs-12 np text-center">
+							<p>
+							Within a month, this quiz generated more than <b>72K visits, 23K leads, and 5K engagements. </b><br/>
+							The TAG Livros quiz engaged and entertained their audience and thus was a huge hit on social media. They posted on their Facebook page and ran targeted Facebook Ads.<br/>
+							<b>You will see one of their Facebook posts below, which generated over 900 likes/reactions and over 120 shares. </b> <br/>
+							<b>Overall, they generated more than 55K leads and 196K visits in traffic.</b> And the interactive experience became a huge hit!
+							</p>
+						</div>
+						<div class="col-md-12 col-sm-12 col-xs-12 np text-center img-wrapper">
+							<img class="" src="{{ $page->baseUrl }}/images/case_study_result1.jpg" alt="Result img">
+							<img class="" src="{{ $page->baseUrl }}/images/case_study_result2.jpg" alt="Result img">
+						</div>
+					</div>
+				</section>
+
 			</div>
 			
 			<div id="getPaid" class="tab-pane fade">
