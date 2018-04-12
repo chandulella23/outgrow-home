9a9241ecd59ca99f10654c8fafedeb777ea52b63
Arti <arti.thind@venturepact.com>
2018-04-12 11:19:20 +0530

    EU-GDPR page HTML

---
+++ source/EU-GDPR.blade.php
@@ -0,0 +1,328 @@
+@extends('_layouts.master')
+
+@section('title', 'EU-GDPR | Outgrow')
+
+@section('metaDescription')
+	<meta name="description" content="EU-GDPR, General Data Protection Regulation, commonly referred to as GDPR, is a new piece of European Union legislation intended to standardize data regulation across Europe while providing greater protection and control over data to the consumer." />
+	<link rel="canonical" href="http://outgrow.co/EU-GDPR.html" />
+	<meta property="og:locale" content="en_GB" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="EU-GDPR | Outgrow" />
+	<meta property="og:description" content="EU-GDPR, General Data Protection Regulation, commonly referred to as GDPR, is a new piece of European Union legislation intended to standardize data regulation across Europe while providing greater protection and control over data to the consumer." />
+	<meta property="og:url" content="https://outgrow.co/EU-GDPR.html" />
+	<meta property="og:site_name" content="Outgrow" />
+	<meta property="og:image" content="https://cdn.filestackcontent.com/dIn85YuTWuak2ghxNu89" />
+	<meta name="twitter:image" content="https://cdn.filestackcontent.com/dIn85YuTWuak2ghxNu89" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:description" content="EU-GDPR, General Data Protection Regulation, commonly referred to as GDPR, is a new piece of European Union legislation intended to standardize data regulation across Europe while providing greater protection and control over data to the consumer." />
+	<meta name="twitter:title" content="EU-GDPR | Outgrow" />
+	<meta name="twitter:creator" content="@outgrowco" />
+@endsection
+
+@section('inlinescripts')
+@endsection
+
+@section('css')
+	<link rel="stylesheet" href="{{ $page->baseUrl }}/css/bootstrap.css">
+	<link rel="stylesheet" href="{{ $page->baseUrl }}/css/custom-sa.css">
+	<link rel="stylesheet" href="{{ $page->baseUrl }}/css/sahil-hover.css">
+	<link rel="stylesheet" href="{{ $page->baseUrl }}/css/home-responsive.css">
+	<link rel="stylesheet" href="{{ $page->baseUrl }}/css/why-responsive.css">
+	<link rel='stylesheet' href='{{ $page->baseUrl }}/css/stylesheet.min.css' type='text/css' media='all'  />
+	<link rel='stylesheet' href='{{ $page->baseUrl }}/css/js_composer.mina752.css?ver=4.11.2.1' type='text/css' media='all' />
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+@endsection
+
+@section('pageClass', 'Privacy loaded')
+
+@section('pageId', '')
+
+@section('content')
+	<section class="section section-services">
+		<div class="container res-container">
+			<div class="Privacy-heading">
+				EU-GDPR
+			</div>
+			<h4 class="Privacy-subheading">
+				What is GDPR?
+			</h4>
+			<p>
+			General Data Protection Regulation, commonly referred to as GDPR, is a new piece of European Union legislation intended to standardize data regulation across Europe while providing greater protection and control over data to the consumer. This legal framework replaces the current EU Data Protection Directive (95/46/EC) with additional requirements that you need to be aware of. The new EU data protection regime extends the scope of the EU data protection law to all companies even outside the EU when they process data of EU residents.
+			</p>
+			<h4 class="Privacy-subheading">Who does GDPR applies to?</h4>
+			<p>
+				GDPR applies to persons and entities of all sizes that process personal data of EU residents, regardless of where they are based. These regulations apply to both data controllers and data processors, including third parties such as cloud providers. It applies to all 28 EU member states and to entities and organisations outside the EU when processing the data of citizens within it.
+			</p>
+			<h4 class="Privacy-subheading">What is personal data according to GDPR?</h4>
+			<p>
+				Anything that contributes or links to identifying an individual will be included, whether it relates to his or her private, professional or public life. It can be anything from a name, a home address, a photo, an email address, bank details, posts on social networking websites, medical information, a computer’s IP address and includes both biometric and genetic data. 
+			</p>
+			
+			<h4 class="Privacy-subheading">GDPR Requirements</h4>
+			<ul>
+				<li class="li-style">
+					<b>Consent</b> - Unbundled consent as well as purpose for using the data, should be stored in an auditable and secured way. (Part of article 7 and 8 of the regulation). Consent related guidelines include rights of access, to be forgotten, and to data portability. Data Controllers and Data Processors have an obligation to clearly communicate these rights to individuals they collect data from. Companies will no longer be able to use long illegible T&Cs full of legalese, as the request for consent must be given in an intelligible and easily accessible form, with the purpose for data processing attached to that consent. Consent now must be clear and distinguishable from other matters and provided in an intelligible and easily accessible form, using clear and plain language. It must be as easy to withdraw consent as it is to give it.
+				</li>
+				<li class="li-style">
+					<b>Data access and rectification</b> - Data subject should have access to collected data. Data subject should be aware if their data is being transferred to other countries. They should be able to download and ask for modification of the collected data.  (Part of article 15 and 16)
+				</li>
+				<li class="li-style">
+					<b>Erasure - Right to be forgotten</b> - Gives a data subject right to ask the organization (controller) to forget or erase personal data. (Part of article 17) 
+				</li>
+				<li class="li-style">
+					<b>Data storage</b> - Store EU data within EU separate from other countries 
+				</li>
+				<li class="li-style">
+					<b>Data protection and security</b> - Required by design, this includes secure storage of data including encryption, data minimization and controlled access to data and applications. (Part of article 25 and 32)
+				</li>
+			</ul>
+			<h4 class="Privacy-subheading">Why comply?</h4>
+			<p>
+				This regulation will affect anyone gathering data of users from EEA region. Our clients need to comply when creating interactive experiences using Outgrow to engage audience and gather leads and other data from users in EEA region– as the potential fines up €20 million (approx. $23 million USD) can be levied on anyone not complying in the handling of an individual’s personal information.
+			</p>
+			<p>
+				Even if you don’t operate in EEA, you may still be liable under the GDPR – unless you are actively preventing any Europeans from visiting the page where you embed the Outgrow interactive experience. You can be fined by the EU for serving a non-GDPR compliant site to Europeans – even if you have no intention to target them. 
+			</p>
+			<h4 class="Privacy-subheading">Outgrow and EU GDPR Compliance </h4>
+			<p>
+				Over the past year we are preparing to meet the requirements of the GDPR, the new data protection law coming into force on 25 May 2018. The GDPR affects European and non-European businesses using online advertising and data gathering when their sites and apps are accessed by users in the European Economic Area <a class="text-red" href="http://www.efta.int/eea"> (EEA).</a>
+			</p>
+			<p>
+				On this page you can review and download our GDPR related documentation- including our <a class="text-red" href="https://docs.google.com/document/d/17A2SC9eiJcAA6eUFaMbfWDj5CtPPKroSmSb2HLUFXt4/edit"> updated EU User Consent Policy</a>, changes to our contract terms, and changes to our products, to help both you and Outgrow meet the new requirements.
+			</p>
+			<h4 class="Privacy-subheading">How is data handled at Outgrow? </h4>
+			<p>
+				We highly value our customers data privacy and protection. Please have a look at our <a class="text-red" href="https://outgrow.co/privacy-policy/?utm_source=footer">Privacy Policy </a> for more information on how we manage, process and store your data submitted in the context of providing our services.
+			</p>
+			<p>
+				We classified the policy in six parts :
+			</p>
+			<ul>
+				<li class="li-style">
+					Personal data collected 
+				</li>
+				<li class="li-style">
+					Third party data 
+				</li>
+				<li class="li-style">
+					Data retention periods
+				</li>
+				<li class="li-style">
+					Location of data storage and transfers
+				</li>
+				<li class="li-style">
+					Security
+				</li>
+				<li class="li-style">
+					Privacy policy changes
+				</li>
+			</ul>
+			<p>
+				We also provide specific Data Protection Agreements to our clients who would be the Data Controllers. You can contact us on <a class="text-red" href="https://outgrow.co/privacy-policy/?utm_source=footer"> privacy@outgrow.co </a> should you wish to obtain this DPA.
+			</p>
+			<h4 class="Privacy-subheading">Updated EU User Consent Policy</h4>
+			<p>
+				Outgrow's EU User Consent Policy is being updated to reflect the new legal requirements of the GDPR. It sets out your responsibilities for making disclosures to, and obtaining consents from, end users in the EEA. For example, under that policy, our clients will be required to obtain consent from users for the collection of data through the interactive experiences they create and for the use of cookies where legally required. 
+			</p>
+			<h4 class="Privacy-subheading">Product changes </h4>
+			<p>
+				To comply, and support your compliance with GDPR, we are:<br/>
+				Making some changes for the clients who request GDPR compliance- 
+			</p>
+			<ul>
+				<li class="li-style">
+					Enabling publishers to show cookie accept/ deny pop up on the Outgrow interactive experience.
+				</li>
+				<li class="li-style">
+					Taking steps to limit the processing of personal information for children under the GDPR Age of Consent in individual member states.
+				</li>
+				<li class="li-style">
+					Work on data retention practices; and launching new controls for Outgrow clients to manage the retention and deletion of their data.
+				</li>
+				<li class="li-style">
+					Including single opt in and double opt in for gathering consent from end users while using Outgrow interactive experiences.
+				</li>
+				<li class="li-style">
+					No individual or personal data from end users is being stored by Outgrow – unless you are using lead form,  in which case you are also covered as the data is stored in your own private webspace, not accessible by anyone but you and hosted in a secure European data center.
+				</li>
+				<li class="li-style">
+					When you use Outgrow to build an interactive experience you are creating your content in your own Outgrow builder and your user’s data is stored in your private space. No one else has access to that data. This means, you are fully responsible for how you use Outgrow. Our toolkit provides you with everything you need to keep your interactive experiences compliant with GDPR, but if you are based outside of Europe and have no European visitors on your sites, you might not need all these warnings and messages, so we are giving you the option to not show them.
+				</li>
+				<li class="li-style">
+					All Outgrow servers are EU-based. Our data center is ISO 27001 certified.
+				</li>
+				<li class="li-style">
+					Users data tracking using Analytics – Our clients have a choice of limiting Google Analytics and Facebook tracking by giving users an option to accept or reject cookies and tracking.
+				</li>
+				<li class="li-style">
+					We will be rolling out a data processing agreement, which you need to sign if you need us to access your account and your data to help you out with technical support. We will have that ready for you soon.
+				</li>
+				<li class="li-style">
+					We also allow complete transfer (to your CRM) and erasure of data from Outgrow to comply with Right to be forgotten.
+				</li>
+				<li class="li-style">
+					We are also giving an option to our clients to delete a selected record completely from Outgrow.
+				</li>
+				<li class="li-style">
+					There will be an option to share the data of an end user of Outgrow (in a downloadable format).
+				</li>
+				<li class="li-style">
+					We will be giving a link on Outgrow settings section, where users can request GDPR compliance by ticking a checkbox box. 
+				</li>
+			</ul>			
+			<h4 class="Privacy-subheading">
+				Self-help checklist on data protection policy
+			</h4>
+			<p>
+				We recommended all our clients to follow this checklist before launching an experience. If you can answer a Yes to all of these question, your business is in good shape from a data protection viewpoint. If you don't have a clean sheet, the checklist can help you identify the areas where you need to improve the interactive experience.
+			</p>
+			<h5>Rule 1: Fair obtaining:</h5>
+			<ul>
+				<li class="li-style">
+					At the time when you collect information about individuals on your interactive experience, are they made aware of the uses for that information?
+				</li>
+				<li class="li-style">
+					Are people made aware of any disclosures of their data to third parties?
+				</li>
+				<li class="li-style">
+					Have you obtained consent for any secondary uses of their personal data, which might not be obvious to them?
+				</li>
+				<li class="li-style">
+					Can we describe our data-collection practices as open, transparent and up-front?
+				</li>
+			</ul>
+			<h5>Rule 2: Purpose specification</h5>
+			<ul>
+				<li class="li-style">
+					Are you clear about the purpose (or purposes) for which you keep the personal information of users?
+				</li>
+				<li class="li-style">
+					Are the individuals on your database also clear about this purpose?
+				</li>
+				<li class="li-style">
+					If you are required to register with the Data Protection Commissioner, does your register entry include a proper, comprehensive statement of your purpose? [Remember, if you are using personal data for a purpose not listed on your register entry, you may be committing an offence.] Has responsibility been assigned for maintaining a list of all data sets and the purpose associated with each?
+				</li>
+				<h5>Rule 3: Use and disclosure of information</h5>
+				<li class="li-style">
+					Are there defined rules about the use and disclosure of information?
+				</li>
+				<li class="li-style">
+					Are all team members aware of these rules?
+				</li>
+				<li class="li-style">
+					Are the individuals aware of the uses and disclosures of their personal data? Would they be surprised if they learned about them? Consider whether the consent of the individuals should be obtained for these uses and disclosures.
+				</li>
+				<li class="li-style">
+					If you are required to register with the Data Protection Commissioner, does your register entry include a full list of persons or business to whom you may need to disclose personal data? [Remember, if you disclose personal data to someone not listed on your register entry, you may be committing an offence.]
+				</li>
+			</ul>
+			<h5>Rule 4: Security</h5>
+			<ul>
+				<li class="li-style">
+					Is there a list of security provisions in place for each data set?
+				</li>
+				<li class="li-style">
+					Is someone responsible for the development and review of these provisions?
+				</li>
+				<li class="li-style">
+					Are these provisions appropriate to the sensitivity of the personal data we keep?
+				</li>
+				<li class="li-style">
+					Are your computers and your databases password-protected, and encrypted if appropriate?
+				</li>
+				<li class="li-style">
+					Are your computers, servers, and files securely locked away from unauthorised people?
+				</li>
+			</ul>
+			<h5>Rule 5: Adequate, relevant and not excessive</h5>
+			<ul>
+				<li class="li-style">
+					Do you collect all the information you need to serve our purpose effectively, and to deal with individuals in a fair and comprehensive manner?
+				</li>
+				<li class="li-style">
+					Have you checked to make sure that all the information you collect is relevant, and not excessive, for your purpose?
+				</li>
+				<li class="li-style">
+					If an individual asked you to justify every piece of information you hold about him or her, could you do so?
+				</li>
+				<li class="li-style">
+					Does a policy exist in this regard?
+				</li>
+			</ul>
+			<h5>Rule 6: Adequate, relevant and not excessive</h5>
+			<ul>
+				<li class="li-style">
+					Do you check your data for accuracy?
+				</li>
+				<li class="li-style">
+					Do you know how much of your personal data is time-sensitive, i.e. likely to become inaccurate over time unless it is updated?
+				</li>
+				<li class="li-style">
+					Do you take steps to ensure your databases are kept up-to-date?
+				</li>
+			</ul>
+			<h5>Rule 7: Retention time</h5>
+			<ul>
+				<li class="li-style">
+					Is there a clear statement on how long items of information are to be retained?
+				</li>
+				<li class="li-style">
+					Are you clear about any legal requirements on you to retain data for a certain period?
+				</li>
+				<li class="li-style">
+					Do you regularly purge your databases of data which you no longer need, such as data relating to former customers or staff members?
+				</li>
+				<li class="li-style">
+					Do you have a policy on deleting personal data as soon as the purpose for which we obtained the data has been completed?
+				</li>
+			</ul>
+			<h5>Rule 8: The Right of Access</h5>
+			<ul>
+				<li class="li-style">
+					Is a named individual responsible for handling access requests?
+				</li>
+				<li class="li-style">
+					Are there clear procedures in place for dealing with such requests?
+				</li>
+				<li class="li-style">
+					Do these procedures guarantee compliance with the Act's requirements?
+				</li>
+			</ul>
+			<h4 class="Privacy-subheading">Training & Education</h4>
+			<ul>
+				<li class="li-style">
+					Do you know about the levels of awareness of data protection in your organisation?
+				</li>
+				<li class="li-style">
+					Is your staff aware of their data protection responsibilities - including the need for confidentiality?
+				</li>
+				<li class="li-style">
+					Is data protection included as part of the training programme for your staff?
+				</li>
+			</ul>
+			<h4 class="Privacy-subheading">Co-ordination and Compliance</h4>
+			<ul>
+				<li class="li-style">
+					Has a data protection co-ordinator and compliance person been appointed?
+				</li>
+				<li class="li-style">
+					Are your staff members aware of their role?
+				</li>
+				<li class="li-style">
+					Are there mechanisms in place for formal review by the co-ordinator of data protection activities within your organisation?
+				</li>
+			</ul>
+			<p>
+				If you have any questions about this update, please don't hesitate to reach out to your account manager or contact us through the Help Center. We will continue to share further information on our plans in the coming weeks.
+			</p>
+
+		</div>
+	</section>
+@endsection
+
+@section('pageScripts')
+	const libs = {
+		"jquery": "https://code.jquery.com/jquery-2.1.4.min.js",
+		"sitemin": "{{ $page->baseUrl }}/js/site.min.js"
+	}
+@endsection
