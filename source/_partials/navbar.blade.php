bae798c0fac2e7a1de60689545642ae401d7e666
Arti <arti.thind@venturepact.com>
2018-05-03 17:27:03 +0530

    fixes

--- source/_partials/navbar.blade.php
+++ source/_partials/navbar.blade.php
@@ -33,10 +33,10 @@
 					<div class="drop-sub-menu">
 						<a href="{{ $page->baseUrl }}/case-studies" onclick="callGA('CASESTUDIES')" class="params" id="nav-case-studies">Success Stories <i class="material-icons">chevron_right</i></a>
 						<div class="dropdown-sub-content">
-							<a href="{{ $page->baseUrl }}/case-studies#tag" class="nav-case">TAG </a>
-							<a href="{{ $page->baseUrl }}/case-studies#getPaid" class="nav-case">Get Paid Your Pad </a>
-							<a href="{{ $page->baseUrl }}/case-studies#vp" class="nav-case">VenturePact </a>
-							<a href="{{ $page->baseUrl }}/case-studies#macroscape" class="nav-case">Macroscape </a>
+							<a href="#tag" class="nav-case">TAG </a>
+							<a href="#getPaid" class="nav-case">Get Paid Your Pad </a>
+							<a href="#vp" class="nav-case">VenturePact </a>
+							<a href="#macroscape" class="nav-case">Macroscape </a>
 						</div>
 					</div>
 				</div>
