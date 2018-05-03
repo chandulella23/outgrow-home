42ee55e87dd52d22e957b334008b75969ca0d648
Arti <arti.thind@venturepact.com>
2018-05-03 12:56:37 +0530

    fixes

--- source/js/pageScripts/case-studies.js
+++ source/js/pageScripts/case-studies.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function () {
     jQuery('.counter').counterUp();
+    jQuery('#nav-case-studies').addClass('active');
+    jQuery('#nav-why-interactive').removeClass('active');
 });
 
