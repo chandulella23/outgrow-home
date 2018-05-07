ddb5073b84a9d59f83877da341c918d5bef03da5
Arti <arti.thind@venturepact.com>
2018-05-07 09:40:16 +0530

    case study button links

--- source/pricing.blade.php
+++ source/pricing.blade.php
@@ -1134,12 +1134,12 @@
                         <h5>Create Custom </h5>
                         <span class="calc">Calculators/Quiz/Outcome/Poll</span>
                     </div>
-                    <div class="col-md-6 col-sm-6 col-xs-12 np text-center create-exp-center">
-                        <span class="create-exp-start">Starting From </span> &nbsp;
+                    <div class="col-md-7 col-sm-7 col-xs-12 np text-center create-exp-center">
+                        <span class="create-exp-start">Starting From </span>
                         <span class="create-exp-price">$250</span>
                         <span class="text-opacity">/content pieces</span>
                     </div>
-                    <div class="col-md-3 col-sm-3 col-xs-12 np">
+                    <div class="col-md-2 col-sm-2 col-xs-12 np">
                         <button class="btn btn-white-red-outline btn-hover intercom_trigger" id="intercom_trigger">CLICK HERE</button>
                     </div>
                     <!-- <button class="closeDiv"><i class="material-icons">cancel</i></button> -->
