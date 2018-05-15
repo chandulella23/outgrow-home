fc4d491a6a9989f535346014dc73a77ce5af9492
Arti <arti.thind@venturepact.com>
2018-05-15 17:13:44 +0530

    added fancy navbar files

---
+++ source/js/pageScripts/navbar.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function () {
+
+    jQuery("#menu-close").click(function(e) {
+        e.preventDefault();
+        jQuery("#sidebar-wrapper").toggleClass("active");
+        jQuery('.overlay').fadeOut("slow");
+        jQuery("html,body").css({"overflow": "auto"});
+    });
+    jQuery("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        jQuery("#sidebar-wrapper").toggleClass("active");
+        jQuery('.overlay').fadeIn("slow");
+        jQuery("html,body").css({"overflow": "hidden"});
+    });
+
+    jQuery('.overlay').click(function (e) {
+        jQuery('#sidebar-wrapper').removeClass('active');
+        jQuery('.overlay').fadeOut("slow");
+        jQuery("html,body").css({"overflow": "auto"});
+    });
+    jQuery('.nav-case').click(function (e) {
+        jQuery('#sidebar-wrapper').removeClass('active');
+        jQuery('.overlay').fadeOut("slow");
+        jQuery("html,body").css({"overflow": "auto"});
+    });
+
+
+})
