9b4815d2084e5f301e454d73b78fc87f4c4d3938
Rahul kumar <rahul.kumar@venturepact.com>
2018-11-21 18:15:45 +0530

    Open intercom on sumome

--- source/js/pageScripts/navbar.js
+++ source/js/pageScripts/navbar.js
@@ -1,22 +1,22 @@
 jQuery(document).ready(function () {
 
-    jQuery("#menu-close").click(function(e) {
+    jQuery("#menu-close").click(function (e) {
         e.preventDefault();
         jQuery("#sidebar-wrapper").removeClass("active");
         jQuery('.overlay').fadeOut("slow");
-        jQuery("html,body").css({"overflow": "auto"});
+        jQuery("html,body").css({ "overflow": "auto" });
     });
-    jQuery("#menu-toggle").click(function(e) {
+    jQuery("#menu-toggle").click(function (e) {
         e.preventDefault();
         jQuery("#sidebar-wrapper").addClass("active");
         jQuery('.overlay').fadeIn("slow");
-        jQuery("html,body").css({"overflow": "hidden"});
+        jQuery("html,body").css({ "overflow": "hidden" });
     });
 
     jQuery('.overlay').click(function (e) {
         jQuery('#sidebar-wrapper').removeClass('active');
         jQuery('.overlay').fadeOut("slow");
-        jQuery("html,body").css({"overflow": "auto"});
+        jQuery("html,body").css({ "overflow": "auto" });
     });
     // jQuery('.nav-case').click(function (e) {
     //     jQuery('#sidebar-wrapper').removeClass('active');
@@ -26,12 +26,13 @@ jQuery(document).ready(function () {
 
     // jQuery('#nav-get-inspired').addClass('active');
     // jQuery('#nav-use-cases').addClass('active');
-
     jQuery("#drop-toggle").click(function (e) {
         e.preventDefault();
         jQuery(".dropdown-content").css({ "display": "block" });
         jQuery(".dropdown-sub-content").css({ "display": "block" });
         jQuery('#GetInspired').addClass('open');
     });
+
+
 
 })
