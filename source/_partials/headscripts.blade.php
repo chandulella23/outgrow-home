235f2a681eba55950a1bdc29ad34e5830951d2d4
Rahul Kumar <rahulkumar2048@gmail.com>
2019-02-01 12:52:55 +0530

    remove heatmap script

--- source/_partials/headscripts.blade.php
+++ source/_partials/headscripts.blade.php
@@ -119,12 +119,6 @@ v.parentNode.insertBefore(j,v)})(window,document,'script','//load.sumo.com/');
     parser.href = window.location.href;
 	ga('create', gap, 'auto', 'markettingteam');
 	
-	(function(h,e,a,t,m,p) {
-      m=e.createElement(a);m.async=!0;m.src=t;
-      p=e.getElementsByTagName(a)[0];p.parentNode.insertBefore(m,p);
-    })(window,document,'script','https://u.heatmap.it/log.js');
-
-	
 </script>
 <!-- Analytics Tracking Snippets Ends -->
 <style>
