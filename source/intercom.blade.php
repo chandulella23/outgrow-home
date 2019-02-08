fef293e578ef197b301569284d9e88d65dcce2bb
Shagun-29 <shagun.sharma@venturepact.com>
2019-02-08 13:06:38 +0530

    add intercom page

---
+++ source/intercom.blade.php
@@ -0,0 +1,34 @@
+@extends('_layouts.lp')
+
+@section('title', 'Increase Online Sales | Outgrow')
+
+@section('metaDescription')
+	<meta name="description" content="Increase your online sales with interactive quizzes, calculators and assessments. Build your first interactive experience. Start your free trial."/>
+	<link rel="canonical" href="http://outgrow.co/" />
+	<meta property="og:locale" content="en_GB" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Interactive Calculators | Outgrow" />
+	<meta property="og:description" content="Increase your online sales with interactive quizzes, calculators and assessments. Build your first interactive experience. Start your free trial." />
+	<meta property="og:url" content="https://outgrow.co/" />
+	<meta property="og:site_name" content="Outgrow" />
+	<meta property="og:image" content="https://cdn.filestackcontent.com/dIn85YuTWuak2ghxNu89" />
+	<meta name="twitter:image" content="https://cdn.filestackcontent.com/dIn85YuTWuak2ghxNu89" />
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:description" content="Increase your online sales with interactive quizzes, calculators and assessments. Build your first interactive experience. Start your free trial." />
+	<meta name="twitter:title" content="Interactive Calculators | Outgrow" />
+	<meta name="twitter:creator" content="@outgrowco" />
+@endsection
+
+@section('css')
+	<link rel="stylesheet" href="{{ $page->baseUrl }}/css/allPage_minified.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="{{ $page->baseUrl }}/css/navbar.css">
+	<link rel="stylesheet" href="https://unpkg.com/simplebar@latest/dist/simplebar.css" />
+@endsection
+
+@section('pageClass', '')
+
+@section('pageId', '')
+
+@section('content')
+
